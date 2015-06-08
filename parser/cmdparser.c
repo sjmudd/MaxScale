954cb3ce011a8c2d13b3b6320aeecb31d29891a4
Markus Makela <markus.makela@mariadb.com>
2015-06-08 14:33:27 +0300

    Added a command line parsing tool

---
+++ parser/cmdparser.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <parser.h>
+
+int main(int argc, char** argv)
+{
+    struct parser_token_t* head = NULL;
+    char* argstr;
+    int arglen = 1;
+
+    if(argc < 2)
+    {
+        printf("Usage: %s STRING\n",argv[0]);
+        return 1;
+    }
+
+    for(int i = 1;i<argc;i++)
+    {
+        arglen += strlen(argv[i]) + 1;
+    }
+
+    argstr = malloc(arglen);
+    *argstr = '\0';
+
+
+    for(int i = 1;i<argc;i++)
+    {
+        strcat(argstr,argv[i]);
+        strcat(argstr," ");
+    }
+    argstr[strlen(argstr)-1] = '\0';
+    printf("Parsing: '%s'\n",argstr);
+    tokenize_string(&head,(char*)argstr,strlen(argstr));
+    if(head == NULL)
+    {
+        printf("Error: parsing failed.\n");
+        return 1;
+    }
+    printf("Printing tokens...\n");
+    print_all_tokens(stdout,head);
+    printf("\n");
+    free_all_tokens(head);
+ 
+    return 0;
+}
