d12e9ec3d03cd229f8765dd6ef3a289d3484e1b3
Markus Makela <markus.makela@mariadb.com>
2015-07-23 10:34:04 +0300

    Fixed negative current connection count.

--- server/core/dcb.c
+++ server/core/dcb.c
@@ -558,8 +558,6 @@ dcb_process_victim_queue(DCB *listofdcb)
             }  
             else 
             {
-		if(dcb->server)
-		    atomic_add(&dcb->server->stats.n_current,-1);
                 dcb->fd = DCBFD_CLOSED;
 				
                 LOGIF(LD, (skygw_log_write_flush(
